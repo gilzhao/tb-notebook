bdb3ede9ca11cb7152fd9ae58b12abffc3960f8e
Zhao Yu <gil@MacBook-Pro.local>
2020-08-11 05:06:02 +0800

    add new  function

--- algorithms/03.js
+++ algorithms/03.js
@@ -1,3 +1,32 @@
+const array = new Array(10);
+let count = 10;
+function insert(val) {
+  if (count == array.length) {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i]
+    }
+    array[0] = sum;
+    count = 1;
+  }
+
+  array[count] = val
+  count++;
+}
+
+
+function find(array, x) {
+  let i = 0;
+  let pos = -1;
+  for (; i < array.length; i++) {
+    if (array[i] == x) {
+      pos = i;
+      break
+    }
+  }
+  return pos;
+}
+
 function cal2(n) {
 	let sum = 0;
 	let i = 1;
@@ -21,4 +50,4 @@ function cal(n) {
 }
 
 
-console.log(cal2(2))
+console.log(find([1,3,4,5,6]),5)
